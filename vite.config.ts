55e672bb4f6bc0b78b0ea636bfe03b9cb402a276
Julius Ossim <juliusjacksonossim@gmail.com>
2023-05-14 13:27:31 +0100

    project trimmed, setup configs complete, not running

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react({
+    babel: {
+      plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+    },
+  })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
+  
