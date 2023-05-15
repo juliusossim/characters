5ed9239e04e50e9dbbc1c6f14f64ae0f2ac96138
Julius Ossim <juliusjacksonossim@gmail.com>
2023-05-15 02:32:04 +0100

    url parsed directly

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,23 +1,23 @@
-import { config } from '@/config';
+// import { config } from '@/config';
 import Axios, { AxiosRequestConfig } from 'axios';
 
 export const axios = Axios.create({
-  baseURL: config.restBase,
-  timeout: 5000
+	baseURL: 'https://finalspaceapi.com/api/v0/character/',
+	timeout: 5000,
 });
 
 const token = sessionStorage.getItem('token');
 
 if (token) {
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+	axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 }
 
 export const apiRequest = async <T>(config: AxiosRequestConfig): Promise<T> => {
-  try {
-    const response = await axios(config);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+	try {
+		const response = await axios(config);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
 };
